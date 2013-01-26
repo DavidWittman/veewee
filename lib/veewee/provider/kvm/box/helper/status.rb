7a2de7f10587c67ceba942cb7b3a25fa691a340f
David Wittman <david.wittman@rackspace.com>
2013-01-26 14:29:09 -0600

    [KVM] Use filters for exists_vm and exists_volume
    Rather than iterating over each domain/volume, fog allows you to
    pass a filter to list_domains or list_volumes. Using this method
    can prevent unnecessary Libvirt::RetrieveErrors from being thrown
    because we're not touching all domains/volumes during retrieval.

--- lib/veewee/provider/kvm/box/helper/status.rb
+++ lib/veewee/provider/kvm/box/helper/status.rb
@@ -15,11 +15,11 @@ module Veewee
         end
 
         def exists_volume?
-          @connection.list_volumes.find { |v| v[:name] == "#{name}.img" }
+          @connection.list_volumes(:name => "#{name}.img")
         end
 
         def exists_vm?
-          @connection.list_domains.find { |d| d[:name] == name }
+          @connection.list_domains(:name => name)
         end
 
       end # End Module
